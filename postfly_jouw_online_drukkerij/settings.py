86d1881dc66d5f47e98dd9dd30ea4ee001d60528
Daph1986 <daphne.heimgartner@gmail.com>
2021-09-15 12:24:13 +0200

    Setup blocks in base and added home app and templates

--- postfly_jouw_online_drukkerij/settings.py
+++ postfly_jouw_online_drukkerij/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +65,10 @@ ROOT_URLCONF = 'postfly_jouw_online_drukkerij.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
