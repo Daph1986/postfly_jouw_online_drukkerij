6af6cafda816699e00b54267a6fa3c6b7b89e001
Daph1986 <daphne.heimgartner@gmail.com>
2021-09-26 12:42:55 +0200

    Added search functionality and updated products fixture

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
@@ -7,9 +9,21 @@ def all_products(request):
 	"""View to return all products"""
 
 	products = Product.objects.all()
+	query = None
+
+	if request.GET:
+		if 'q' in request.GET:
+			query = request.GET['q']
+			if not query:
+				messages.error(request, "No search criteria has been entered!")
+				return redirect(reverse('products'))
+
+			queries = Q(name__icontains=query)
+			products = products.filter(queries)
 
 	context = {
 		'products': products,
+		'search_term': query
 	}
 
 	return render(request, 'products/products.html', context)
