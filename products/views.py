ffca35a0dee08044f57ad3e816f7915dd627acd0
Daph1986 <daphne.heimgartner@gmail.com>
2021-09-28 19:43:17 +0200

    Added sort selector and product counter to products page

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category, Size
 
 # Create your views here.
@@ -12,8 +14,24 @@ def all_products(request):
 	query = None
 	categories = None
 	size = None
+	sort = None
+	direction = None
 
 	if request.GET:
+		if 'sort' in request.GET:
+			sortkey = request.GET['sort']
+			sort = sortkey
+			if sortkey == 'name':
+				sortkey = 'lower_name'
+				products = products.annotate(lower_name=Lower('name'))
+			if sortkey == 'category':
+				sortkey = 'category__name'
+			if 'direction' in request.GET:
+				direction = request.GET['direction']
+				if direction == 'desc':
+					sortkey = f'-{sortkey}'
+			products = products.order_by(sortkey)
+
 		if 'category' in request.GET:
 			categories =  request.GET['category'].split(',')
 			products = products.filter(category__name__in=categories)
@@ -33,11 +51,14 @@ def all_products(request):
 			queries = Q(name__icontains=query) | Q(sku__icontains=query)
 			products = products.filter(queries)
 
+	current_sorting = f'{sort}_{direction}'
+
 	context = {
 		'products': products,
 		'search_term': query,
 		'current_categories': categories,
 		'current_size': size,
+		'current_sorting': current_sorting,
 	}
 
 	return render(request, 'products/products.html', context)
