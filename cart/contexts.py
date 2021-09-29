5432f2465f82e7fec650a5d0494a1f119f776dea
Daph1986 <daphne.heimgartner@gmail.com>
2021-09-29 11:03:59 +0200

    Added cart context

---
+++ cart/contexts.py
@@ -0,0 +1,16 @@
+from decimal import Decimal
+from django.conf import settings
+
+def cart_contents(request):
+
+	cart_items = []
+	total = 0
+	product_count = 0
+
+	context = {
+		'cart_items': cart_items,
+        'total': total,
+        'product_count': product_count,
+	}
+
+	return context
