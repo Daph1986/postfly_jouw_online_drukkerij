32a4a8c16ec0262b2e69d1395b4438fc10570124
Daph1986 <daphne.heimgartner@gmail.com>
2021-09-30 22:20:51 +0200

    Changed item_id to custom id and corrected bag to cart

--- cart/views.py
+++ cart/views.py
@@ -7,18 +7,17 @@ def view_cart(request):
     return render(request, 'cart/cart.html')
 
 
-def add_to_cart(request, item_id):
+def add_to_cart(request, product_id):
     """Add a product to the shopping bag"""
 
-    quantity = None
+    quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    if product_id in list(cart.keys()):
+        cart[product_id] += quantity
     else:
-        cart[item_id] = quantity
+        cart[product_id] = quantity
 
     request.session['cart'] = cart
-    print(request.session['cart'])
     return redirect(redirect_url)
