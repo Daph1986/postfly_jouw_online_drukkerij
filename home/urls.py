886e406e74e2a05303c4c14980213819e89fce1e
Daph1986 <daphne.heimgartner@gmail.com>
2021-10-07 11:51:06 +0200

    Added url path, view and content to specification page and updated nav-link

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('aanleveren/', views.specs, name='specs')
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def index(request):
     """View to return index page"""
 
     return render(request, 'home/index.html')
+
+
+def specs(request):
+    """View to return files specifications page"""
+
+    return render(request, 'home/specs.html')
