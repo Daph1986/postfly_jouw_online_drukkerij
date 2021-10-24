260226733da5036597627d11e33d16493e7853dd
Daph1986 <daphne.heimgartner@gmail.com>
2021-10-24 22:02:50 +0200

    Added profile form, views, updated template and added dashboard template

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 def profile(request):
 	""" Displays the user's profile """
 	profile = get_object_or_404(UserProfile, user=request.user)
 
+	if request.method == 'POST':
+		form = UserProfileForm(request.POST, instance=profile)
+		if form.is_valid():
+			form.save()
+			messages.success(request, 'Your profile is updated successfully')
+
+	form = UserProfileForm(instance=profile)
+
 	template = 'profiles/profile.html'
 	context = {
-		'profile': profile,
+		'form': form,
 	}
 
 	return render(request, template, context)
+
+
+def dashboard(request):
+    """View to return files specifications page"""
+
+    return render(request, 'profiles/dashboard.html')
 
