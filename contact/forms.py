5e2b92bc6e814d7cbea068dbc15deee8e6310a16
Daph1986 <daphne.heimgartner@gmail.com>
2021-11-02 17:02:09 +0100

    Setup form, urls and view for quotation page

--- contact/forms.py
+++ contact/forms.py
@@ -73,3 +73,49 @@ class SampleKitForm(forms.Form):
 				self.fields[field].widget.attrs['placeholder'] = placeholder
 			self.fields[field].widget.attrs['class'] = 'stripe-style-input'
 			self.fields[field].label = False
+
+
+class QuotationForm(forms.Form):
+	""" Form for the sample kit request page """
+	first_name = forms.CharField(required=True)
+	last_name = forms.CharField(required=True)
+	email = forms.CharField(required=True)
+	company_name = forms.CharField(required=False)
+	street_address1 = forms.CharField(required=True)
+	street_address2 = forms.CharField(required=False)
+	postcode = forms.CharField(required=True)
+	town_or_city = forms.CharField(required=True)
+	country = CountryField(blank_label='Select country*').formfield()
+	phone_number = forms.CharField(required=False)
+	message = forms.CharField(widget=forms.Textarea, required=True)
+
+	def __init__(self, *args, **kwargs):
+		""" 
+		Sample kit form which is displayed at 
+		sample kit request page with placeholders
+		"""
+		super().__init__(*args, **kwargs)
+		placeholders = {
+			'first_name': 'First name',
+			'last_name': 'Last name',
+			'email': 'Email address',
+			'company_name': 'Company name',
+			'street_address1': 'Street address + house number',
+			'street_address2': 'Additional address information',
+			'postcode': 'Postal code',
+			'town_or_city': 'Town or city',
+			'phone_number': 'Phone number',
+			'message': 'Describe what you want a quote for',
+		}
+
+		self.fields['message'].widget.attrs = {'rows': 8}
+		self.fields['first_name'].widget.attrs['autofocus'] = True
+		for field in self.fields:
+			if field != 'country':
+				if self.fields[field].required:
+					placeholder = f'{placeholders[field]} *'
+				else:
+					placeholder = placeholders[field]
+				self.fields[field].widget.attrs['placeholder'] = placeholder
+			self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+			self.fields[field].label = False
--- contact/views.py
+++ contact/views.py
@@ -7,7 +7,7 @@ from django.template.loader import render_to_string
 
 from profiles.models import UserProfile
 
-from .forms import ContactForm, SampleKitForm
+from .forms import ContactForm, SampleKitForm, QuotationForm
 
 def contact(request):
     """View to return our contact page with the contact form"""
@@ -54,7 +54,7 @@ def contact(request):
 
 
 def sample_kit(request):
-    """View to return our contact page with the contact form"""
+    """View to return the request sample kit page with the form"""
     if request.method == 'GET':
         if request.user.is_authenticated:
             try:
@@ -118,6 +118,64 @@ def sample_kit(request):
     return render(request, template, context)
 
 def quotation(request):
-    """View to return our quotation page"""
+    """View to return our quotation page with the form"""
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                quotation_form = QuotationForm(initial={
+                    'email': profile.user.email,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'country': profile.default_country,
+                    'phone_number': profile.default_phone_number,
+                    }
+                )
+            except UserProfile.DoesNotExist:
+                quotation_form = QuotationForm()
+        else:
+            quotation_form = QuotationForm()
+    else:
+        quotation_form = QuotationForm(request.POST)
+        if quotation_form.is_valid():
+            first_name = quotation_form.cleaned_data['first_name']
+            last_name = quotation_form.cleaned_data['last_name']
+            email = quotation_form.cleaned_data['email']
+            company_name = quotation_form.cleaned_data['company_name']
+            street_address1 = quotation_form.cleaned_data['street_address1']
+            street_address2 = quotation_form.cleaned_data['street_address2']
+            postcode = quotation_form.cleaned_data['postcode']
+            town_or_city = quotation_form.cleaned_data['town_or_city']
+            country = quotation_form.cleaned_data['country']
+            phone_number = quotation_form.cleaned_data['phone_number']
+            message = quotation_form.cleaned_data['message']
+            html_msg = render_to_string(
+                'emails/quotation_email.html',
+                {'first_name': first_name, 'last_name': last_name, \
+                 'message': message, \
+                 'company_name': company_name, \
+                 'street_address1': street_address1, \
+                 'street_address2': street_address2,'postcode': postcode, \
+                 'town_or_city': town_or_city, 'country': country, \
+                 'phone_number': phone_number})
+            try:
+                send_mail(first_name, last_name, settings.EMAIL_HOST_USER, [
+                    email, settings.EMAIL_HOST_USER],
+                    html_message=html_msg, fail_silently=False)
 
-    return render(request, 'contact/quotation.html')
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect(reverse('home'),
+                            messages.success(request, f'Thank you for your \
+                                            request. We will send you a \
+                                            quotation as soon as \
+                                            possible.'))
+    
+    template = 'contact/quotation.html'
+    context = {
+        'quotation_form': quotation_form,
+    }
+
+    return render(request, template, context)
