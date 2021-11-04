cc8a990c9603ed818ea8b4111afa1a5efbc16faf
Daph1986 <daphne.heimgartner@gmail.com>
2021-11-04 20:40:31 +0100

    Start restructure view to send email confirmation without webhook

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,11 @@
+from django.http import request
 from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.views.generic import View
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
@@ -30,101 +34,123 @@ def cache_checkout_data(request):
         return HttpResponse(content=e, status=400)
 
 
+class CheckoutView(View):
 
-def checkout(request):
-    stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    def __init__(self, request):
+        self.request = request
 
-    if request.method == 'POST':
-        cart = request.session.get('cart', {})
-    
-        form_data = {
-            'first_name': request.POST['first_name'],
-            'last_name': request.POST['last_name'],
-            'email': request.POST['email'],
-            'company_name': request.POST['company_name'],
-            'street_address1': request.POST['street_address1'],
-            'street_address2': request.POST['street_address2'],
-            'postcode': request.POST['postcode'],
-            'town_or_city': request.POST['town_or_city'],
-            'country': request.POST['country'],
-            'phone_number': request.POST['phone_number'],
-        }
-        order_form = OrderForm(form_data)
-        if order_form.is_valid():
-            order = order_form.save(commit=False)
-            pid = request.POST.get('client_secret').split('_secret')[0]
-            order.stripe_pid = pid
-            order.original_cart = json.dumps(cart)
-            order.save()
-            for product_id, quantity in cart.items():
-                try:
-                    product = Product.objects.get(id=product_id)
-                    if isinstance(quantity, int):
-                        order_line_item = OrderLineItem(
-                            order=order,
-                            product=product,
-                            quantity=quantity,
-                        )
-                        order_line_item.save()
-                except Product.DoesNotExist:
-                    messages.error(request, (
-                        "One of the products in your cart hasn't been found in our database."
-                        "Please contact us for assistance!")
-                    )
-                    order.delete()
-                    return redirect(reverse('view_cart'))
+    def _send_confirmation_email(self, order):
+        """ Sends the user a confirmation email """
+        client_email = order.email
+        subject = render_to_string(
+			'checkout/confirmation_emails/confirmation_email_subject.txt',
+			{'order': order})
+        body = render_to_string(
+			'checkout/confirmation_emails/confirmation_email_body.txt',
+			{'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
 
-            request.session['save_info'] = 'save_info' in request.POST
-            return redirect(reverse('checkout_success', args=[order.order_number]))
-        else:
-            messages.error(request, 'There was an error with your form. \
-                Please check your information to be sure everything is correct.')
-    else:
-        cart = request.session.get('cart', {})
-        if not cart:
-            messages.error(request, "Your shopping cart is empty at this moment")
-            return redirect(reverse('products'))
-
-        current_cart = cart_contents(request)
-        total = current_cart['grand_total']
-        stripe_total = round(total * 100)
-        stripe.api_key = stripe_secret_key
-        intent = stripe.PaymentIntent.create(
-            amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY,
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [client_email]
         )
 
-        if request.user.is_authenticated:
-            try:
-                profile = UserProfile.objects.get(user=request.user)
-                order_form = OrderForm(initial={
-                    'email': profile.user.email,
-                    'company_name': profile.default_company_name,
-                    'street_address1': profile.default_street_address1,
-                    'street_address2': profile.default_street_address2,
-                    'postcode': profile.default_postcode,
-                    'town_or_city': profile.default_town_or_city,
-                    'country': profile.default_country,
-                    'phone_number': profile.default_phone_number,
-                })
-            except UserProfile.DoesNotExist:
-                order_form = OrderForm()
+    def checkout(self, request):
+        stripe_public_key = settings.STRIPE_PUBLIC_KEY
+        stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+        if request.method == 'POST':
+            cart = request.session.get('cart', {})
+        
+            form_data = {
+                'first_name': request.POST['first_name'],
+                'last_name': request.POST['last_name'],
+                'email': request.POST['email'],
+                'company_name': request.POST['company_name'],
+                'street_address1': request.POST['street_address1'],
+                'street_address2': request.POST['street_address2'],
+                'postcode': request.POST['postcode'],
+                'town_or_city': request.POST['town_or_city'],
+                'country': request.POST['country'],
+                'phone_number': request.POST['phone_number'],
+            }
+            order_form = OrderForm(form_data)
+            if order_form.is_valid():
+                order = order_form.save(commit=False)
+                pid = request.POST.get('client_secret').split('_secret')[0]
+                order.stripe_pid = pid
+                order.original_cart = json.dumps(cart)
+                order.save()
+                for product_id, quantity in cart.items():
+                    try:
+                        product = Product.objects.get(id=product_id)
+                        if isinstance(quantity, int):
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                            )
+                            order_line_item.save()
+                    except Product.DoesNotExist:
+                        messages.error(request, (
+                            "One of the products in your cart hasn't been found in our database."
+                            "Please contact us for assistance!")
+                        )
+                        order.delete()
+                        return redirect(reverse('view_cart'))
+
+                self._send_confirmation_email(order)
+                request.session['save_info'] = 'save_info' in request.POST
+                return redirect(reverse('checkout_success', args=[order.order_number]))
+            else:
+                messages.error(request, 'There was an error with your form. \
+                    Please check your information to be sure everything is correct.')
         else:
-            order_form = OrderForm()
+            cart = request.session.get('cart', {})
+            if not cart:
+                messages.error(request, "Your shopping cart is empty at this moment")
+                return redirect(reverse('products'))
 
-    if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-            Check if you have set it in your environment')
+            current_cart = cart_contents(request)
+            total = current_cart['grand_total']
+            stripe_total = round(total * 100)
+            stripe.api_key = stripe_secret_key
+            intent = stripe.PaymentIntent.create(
+                amount=stripe_total,
+                currency=settings.STRIPE_CURRENCY,
+            )
 
-    template = 'checkout/checkout.html'
-    context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
-    }
+            if request.user.is_authenticated:
+                try:
+                    profile = UserProfile.objects.get(user=request.user)
+                    order_form = OrderForm(initial={
+                        'email': profile.user.email,
+                        'company_name': profile.default_company_name,
+                        'street_address1': profile.default_street_address1,
+                        'street_address2': profile.default_street_address2,
+                        'postcode': profile.default_postcode,
+                        'town_or_city': profile.default_town_or_city,
+                        'country': profile.default_country,
+                        'phone_number': profile.default_phone_number,
+                    })
+                except UserProfile.DoesNotExist:
+                    order_form = OrderForm()
+            else:
+                order_form = OrderForm()
 
-    return render(request, template, context)
+        if not stripe_public_key:
+            messages.warning(request, 'Stripe public key is missing. \
+                Check if you have set it in your environment')
+
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+        }
+
+        return render(request, template, context)
 
 
 def checkout_success(request, order_number):
