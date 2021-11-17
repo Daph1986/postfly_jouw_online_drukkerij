f332836626a7f0eabf21f4060aa900afdc133589
Daph1986 <daphne.heimgartner@gmail.com>
2021-11-17 21:40:00 +0100

    Bugfix error page 404 build up 1

--- postfly_jouw_online_drukkerij/settings.py
+++ postfly_jouw_online_drukkerij/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if 'DEVELOPMENT' in os.environ else False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['postfly-jouw-online-drukkerij.herokuapp.com', '127.0.0.1']
 
