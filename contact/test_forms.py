5bbf0ed48dc05b43ad08e80e5ecd1cb30f88c7ab
Daph1986 <daphne.heimgartner@gmail.com>
2021-11-18 14:06:52 +0100

    Expanded test to all 3 forms

--- contact/test_forms.py
+++ contact/test_forms.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
-from contact.forms import ContactForm
+from contact.forms import ContactForm, SampleKitForm, QuotationForm
 
 
 class TestContactForm(TestCase):
     """
-    Tests the contactform if the required fields
+    Tests the contact form if the required fields
     are indeed required.
     """
 
@@ -32,3 +32,88 @@ class TestContactForm(TestCase):
                          [0], 'This field is required.')
         self.assertEqual(form.errors['message']
                          [0], 'This field is required.')
+
+
+class TestSampleKitForm(TestCase):
+    """
+    Tests the sample kit form if the required fields
+    are indeed required.
+    """
+
+    def test_required_fields(self):
+        form = SampleKitForm({
+            'first_name': '',
+            'last_name': '',
+            'email': '',
+            'street_address1': '',
+            'postcode': '',
+            'town_or_city': '',
+            'country': ''
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('first_name', form.errors.keys())
+        self.assertIn('last_name', form.errors.keys())
+        self.assertIn('email', form.errors.keys())
+        self.assertIn('street_address1', form.errors.keys())
+        self.assertIn('postcode', form.errors.keys())
+        self.assertIn('town_or_city', form.errors.keys())
+        self.assertIn('country', form.errors.keys())
+        self.assertEqual(form.errors['first_name']
+                         [0], 'This field is required.')
+        self.assertEqual(form.errors['last_name']
+                         [0], 'This field is required.')
+        self.assertEqual(form.errors['email']
+                         [0], 'This field is required.')
+        self.assertEqual(form.errors['street_address1']
+                         [0], 'This field is required.')
+        self.assertEqual(form.errors['postcode']
+                         [0], 'This field is required.')
+        self.assertEqual(form.errors['town_or_city']
+                         [0], 'This field is required.')
+        self.assertEqual(form.errors['country']
+                         [0], 'This field is required.')
+
+
+class TestQuotationForm(TestCase):
+    """
+    Tests the quotation form if the required fields
+    are indeed required.
+    """
+
+    def test_required_fields(self):
+        form = QuotationForm({
+            'first_name': '',
+            'last_name': '',
+            'email': '',
+            'street_address1': '',
+            'postcode': '',
+            'town_or_city': '',
+            'country': '',
+            'message': ''
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('first_name', form.errors.keys())
+        self.assertIn('last_name', form.errors.keys())
+        self.assertIn('email', form.errors.keys())
+        self.assertIn('street_address1', form.errors.keys())
+        self.assertIn('postcode', form.errors.keys())
+        self.assertIn('town_or_city', form.errors.keys())
+        self.assertIn('country', form.errors.keys())
+        self.assertIn('message', form.errors.keys())
+        self.assertEqual(form.errors['first_name']
+                         [0], 'This field is required.')
+        self.assertEqual(form.errors['last_name']
+                         [0], 'This field is required.')
+        self.assertEqual(form.errors['email']
+                         [0], 'This field is required.')
+        self.assertEqual(form.errors['street_address1']
+                         [0], 'This field is required.')
+        self.assertEqual(form.errors['postcode']
+                         [0], 'This field is required.')
+        self.assertEqual(form.errors['town_or_city']
+                         [0], 'This field is required.')
+        self.assertEqual(form.errors['country']
+                         [0], 'This field is required.')
+        self.assertEqual(form.errors['message']
+                         [0], 'This field is required.')
+
